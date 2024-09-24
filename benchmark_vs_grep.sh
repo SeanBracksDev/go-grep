fe9a02c5500de8fb9f3d75c0c049ccdb760234e8
sean.brackenborough <sean.brackenborough.dev@gmail.com>
2024-09-24 17:44:35 +0100

    refactor: update to point to test_data

--- benchmark_vs_grep.sh
+++ benchmark_vs_grep.sh
@@ -3,7 +3,7 @@
 && date \
 && git rev-parse --abbrev-ref HEAD \
 && git rev-parse HEAD \
-&& hyperfine 'grep a dummy_data/3M' \
-&& hyperfine 'go-grep a dummy_data/3M' \
+&& hyperfine 'grep a test_data/3M' \
+&& hyperfine 'go-grep a test_data/3M' \
 && echo '===============================') \
 &>> benchmark_vs_grep_stats.log
